6e842a6bfe397c5b0354005cdae1d50666cc9180
joojoo8-19 <hyunju0819@icloud.com>
2025-11-09 16:01:54 +0900

    feature : add box filter buttons in python

--- python/ui.py
+++ python/ui.py
@@ -8,6 +8,7 @@ class Ui:
         self.window.title("Learn 100 Japanese Sentences with Leitner System")
         self.window.config(padx=20, pady=20)
 
+        self.current_box = 1
         self.today_cards = today_cards
         self.current_card_index = 0
         self.current_card = today_cards[self.current_card_index]
@@ -28,20 +29,34 @@ class Ui:
         self.divider = ttk.Separator(orient='horizontal')
         self.divider.grid(column=0, row=1,columnspan=5, pady=10, sticky='ew')
 
+        self.box1_button = tkinter.Button(text="Box 1", command=lambda:self.filter_cards_by_box(1))
+        self.box1_button.grid(column=0, row=2)
+        self.box2_button = tkinter.Button(text="Box 2", command=lambda:self.filter_cards_by_box(2))
+        self.box2_button.grid(column=1, row=2)
+        self.box3_button = tkinter.Button(text="Box 3", command=lambda:self.filter_cards_by_box(3))
+        self.box3_button.grid(column=2, row=2)
+        self.box4_button = tkinter.Button(text="Box 4", command=lambda:self.filter_cards_by_box(4))
+        self.box4_button.grid(column=3, row=2)
+        self.box5_button = tkinter.Button(text="Box 5", command=lambda:self.filter_cards_by_box(5))
+        self.box5_button.grid(column=4, row=2)
+
+        self.divider3 = ttk.Separator(orient='horizontal')
+        self.divider3.grid(column=0, row=3, columnspan=5, pady=10, sticky='ew')
+
         self.card_number = tkinter.Label(text=f"Card {self.current_card_index+1} of {len(self.today_cards)}")
         self.box_status = tkinter.Label(text=f"Box {self.progress[str(self.current_card.id)]}")
-        self.card_number.grid(column=0, row=2)
-        self.box_status.grid(column=4, row=2)
+        self.card_number.grid(column=0, row=4)
+        self.box_status.grid(column=4, row=4)
 
         self.question_canvas = tkinter.Canvas(width=300, height=90)
-        self.question_canvas.grid(column=0, columnspan=5, row=3, pady=5, padx=5)
+        self.question_canvas.grid(column=0, columnspan=5, row=5, pady=5, padx=5)
         self.current_lang = self.question_canvas.create_text(10, 30, text="🇰🇷 Korean", anchor="w")
         self.current_question = (
             self.question_canvas.create_text(10, 60, text=self.current_card.korean, anchor="w")
         )
         
         self.answer_button = tkinter.Button(text="Reveal Answer", command=self.show_answer)
-        self.answer_button.grid(column=0, columnspan=5, row=4)
+        self.answer_button.grid(column=0, columnspan=5, row=6)
 
         self.answer_canvas = tkinter.Canvas(width=300, height=150, highlightthickness=0)
         self.answer_canvas.create_text(10, 30, text="🇯🇵 Japanese", anchor="w")
@@ -58,12 +73,20 @@ class Ui:
         if not self.answer_visible:
             self.answer_visible = True
 
-            self.answer_canvas.grid(column=0, columnspan=5, row=5, pady=5, padx=5)
-            self.divider2.grid(column=0, row=6, columnspan=5, pady=10, sticky='ew')
-            self.correct_button.grid(column=0, columnspan=2, row=7)
-            self.incorrect_button.grid(column=4, columnspan=2, row=7)
+            self.answer_canvas.grid(column=0, columnspan=5, row=7, pady=5, padx=5)
+            self.divider2.grid(column=0, row=8, columnspan=5, pady=10, sticky='ew')
+            self.correct_button.grid(column=0, columnspan=2, row=9)
+            self.incorrect_button.grid(column=4, columnspan=2, row=9)
 
             self.window.update()
+
+    def change_box(self, target:int):
+        self.current_box = target
+
+    def filter_cards_by_box(self, target:int):
+        self.change_box(target)
+        filtered = [card for card in self.today_cards if self.progress[str(card.id)] == self.current_box]
+        print(filtered)
 
     def show_next_card(self):
         if self.current_card_index < len(self.today_cards):
@@ -80,15 +103,15 @@ class Ui:
             self.window.update()
 
     def on_correct(self):
-        if self.progress[self.current_card.id] < 5:
-            self.progress[self.current_card.id] += 1
-            print(f"✅ 정답! → Box {self.progress[self.current_card.id]}")
+        if self.progress[str(self.current_card.id)] < 5:
+            self.progress[str(self.current_card.id)] += 1
+            print(f"✅ 정답! → Box {self.progress[str(self.current_card.id)]}")
         else:
             print("🎉 이미 최고 단계 (Box 5)")
         self.show_next_card()
 
     def on_incorrect(self):
-        self.progress[self.current_card.id] = 1
+        self.progress[str(self.current_card.id)] = 1
         print("❌ 오답 → Box 1로 이동")
         self.show_next_card()
 
